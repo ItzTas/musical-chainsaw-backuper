d908d5c571c4b37dd8eaebda4c9a3ef108a17e36
ItzTas <talessabini2@gmail.com>
2024-07-28 20:39:29 -0300

    feat: file and backuper structs

--- main.go
+++ main.go
@@ -4,7 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"log"
+
+	"github.com/spf13/afero"
 )
+
+type File struct {
+	filePath string
+	fileName string
+	content  []byte
+}
+
+type Backuper struct {
+	writer afero.Fs
+	reader afero.Fs
+	files  []File
+}
 
 func main() {
 	src := flag.String("src", "", "source of the directory to copy")
@@ -15,5 +29,22 @@ func main() {
 		log.Fatal("src and dst params required")
 	}
 
-	fmt.Println(*src, *dst)
+	writer := afero.NewOsFs()
+
+	reader := afero.NewReadOnlyFs(afero.NewOsFs())
+
+	backuper := Backuper{
+		reader: reader,
+		writer: writer,
+		files:  []File{},
+	}
+
+	err := backuper.readDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range backuper.files {
+		fmt.Println(string(file.content))
+	}
 }
