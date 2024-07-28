9ebdba5bf90e873c7ef66ead18c38add1571bbc0
ItzTas <talessabini2@gmail.com>
2024-07-28 20:38:54 -0300

    feat: read_dir created

---
+++ read_dir.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/afero"
+)
+
+func (b *Backuper) readDir(src string) error {
+	files, err := afero.ReadDir(b.reader, src)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		path := src + "/" + file.Name()
+		if file.IsDir() {
+			err := b.readDir(path)
+			if err != nil {
+				log.Printf("Error reading directory %s: %v\n", path, err)
+			}
+			continue
+		}
+		content, err := afero.ReadFile(b.reader, path)
+		if err != nil {
+			log.Printf("Error reading file %s: %v\n", path, err)
+			continue
+		}
+		f := File{
+			filePath: path,
+			fileName: file.Name(),
+			content:  content,
+		}
+		b.files = append(b.files, f)
+	}
+	return nil
+}
